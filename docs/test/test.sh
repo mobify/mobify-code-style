6f16905ef953d9f59c5ee67315200c8d0d404b48
Jeremy Wiebe <jeremy@mobify.com>
2016-11-29 11:23:55 -0800

    Make docs/test/test.sh use lint-md instead of remark cli directly

--- docs/test/test.sh
+++ docs/test/test.sh
@@ -5,13 +5,18 @@
 # The result of running our remark configuration on them is commited as
 # `.expected` files. If we change our configuration, a new output will be
 # expected and these files must be re-generated.
-NPM_BIN=$(npm bin)
 TEST_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+LINT_MD="$TEST_DIR/../bin/lint-md"
 
 # Re-generate `.expected` files with current configuration.
-# "$NPM_BIN/remark" "$TEST_DIR/good.md" --no-color > "$TEST_DIR/good.expected.md" 2>&1
-# "$NPM_BIN/remark" "$TEST_DIR/bad.md" --no-color > "$TEST_DIR/bad.expected.md" 2>&1
+#"$LINT_MD" "$TEST_DIR/good.md" --no-color > "$TEST_DIR/good.expected.md" 2>&1
+#"$LINT_MD" "$TEST_DIR/bad.md" --no-color > "$TEST_DIR/bad.expected.md" 2>&1
 
 # Compare remark output with expected output. Fail on diff.
-diff <("$NPM_BIN/remark" "$TEST_DIR/good.md" --no-color 2>&1) "$TEST_DIR/good.expected.md"
-diff <("$NPM_BIN/remark" "$TEST_DIR/bad.md" --no-color 2>&1) "$TEST_DIR/bad.expected.md"
+diff <("$LINT_MD" "$TEST_DIR/good.md" --no-color 2>&1) "$TEST_DIR/good.expected.md"
+EXIT_GOOD=$?
+diff <("$LINT_MD" "$TEST_DIR/bad.md" --no-color 2>&1) "$TEST_DIR/bad.expected.md"
+EXIT_BAD=$?
+
+# Fail if either diff failed
+exit $EXIT_GOOD || $EXIT_BAD
