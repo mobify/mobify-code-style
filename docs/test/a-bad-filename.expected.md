90f9b143a9efde589ded03db720a86f9fcfc797c
Jeremy Wiebe <jeremy@mobify.com>
2016-11-30 08:46:27 -0800

    Add test for bad filename rule

---
+++ docs/test/a-bad-filename.expected.md
@@ -0,0 +1,5 @@
+
+a-bad-filename.md
+  1:1  warning  Do not start file names with `a`  no-file-name-articles  remark-lint
+
+⚠ 1 warning
--- docs/test/test.sh
+++ docs/test/test.sh
@@ -12,12 +12,25 @@ LINT_MD="$TEST_DIR/../bin/lint-md"
 # Re-generate `.expected` files with current configuration.
 #"$LINT_MD" "$TEST_DIR/good.md" --no-color > "$TEST_DIR/good.expected.md" 2>&1
 #"$LINT_MD" "$TEST_DIR/bad.md" --no-color > "$TEST_DIR/bad.expected.md" 2>&1
+#"$LINT_MD" "$TEST_DIR/a-bad-filename.md" --no-color > "$TEST_DIR/a-bad-filename.expected.md" 2>&1
 
 # Compare remark output with expected output. Fail on diff.
+echo "Testing good.md ..."
 diff <("$LINT_MD" "$TEST_DIR/good.md" --no-color 2>&1) "$TEST_DIR/good.expected.md"
 EXIT_GOOD=$?
+
+echo "Testing bad.md ..."
 diff <("$LINT_MD" "$TEST_DIR/bad.md" --no-color 2>&1) "$TEST_DIR/bad.expected.md"
 EXIT_BAD=$?
 
-# Fail if either diff failed
-exit $EXIT_GOOD || $EXIT_BAD
+echo "Testing a-bad-filename.expected.md ..."
+diff <("$LINT_MD" "$TEST_DIR/a-bad-filename.md" --no-color 2>&1) "$TEST_DIR/a-bad-filename.expected.md"
+EXIT_BAD_FILENAME=$?
+
+# Fail if either diff failed.
+# This if will fall apart quickly as we add more test cases.
+# Need to consider some type of looping, etc possibly.
+if [ "$EXIT_GOOD" -ne "0" ] || [ "$EXIT_BAD" -ne "0" ] || [ "$EXIT_BAD_FILENAME" -ne "0" ]; then
+    echo "One or more tests failed!"
+    exit 1
+fi
