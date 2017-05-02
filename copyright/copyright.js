ed07010c71cf46321d412b678d89748e175b6899
Cdok <cdokurno@mobify.com>
2017-05-02 13:26:45 -0700

    converting from array.forEach loops for copyright
    This commit reverts to using standard for loops instead of using array
    .forEach for iterators

--- copyright/copyright.js
+++ copyright/copyright.js
@@ -14,14 +14,15 @@ const colors = require('colors')
 /* eslint-enable */
 
 // example
-// node copyright.js src bin --verify
+// node copyright.js src bin --verify .js
 
 // debug mode
-// node --inspect --debug-brk copyright.js src bin --verify
+// node --inspect --debug-brk copyright.js src bin --verify .js
 
 const getCopyrightHeader = () => {
     return fs.readFileSync('headers/copyright.js.txt')
 }
+var copyright = fs.readFileSync('headers/copyright.js.txt');
 
 const isCopyrightHeader = (data) => {
     if (data.indexOf('Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved.') >= 0) {
@@ -30,39 +31,28 @@ const isCopyrightHeader = (data) => {
     return false
 }
 
-const writeCopyrightHeader = (path, data) => {
-    fs.writeFile(path, data, (err) => {
+const writeCopyrightHeader = (file, data) => {
+    fs.writeFile(file, data, (err) => {
         if (err) {
             console.log(err)
         }
     })
 }
 
-const checkHeaders = (dir) => {
-    const ext = '.js' // TODO - get extension from user input / command line arg
-
-    glob(`../${dir}/**/*${ext}`, (err, files) => {
-
+const updateHeaders = (folder, ext) => {
+    glob(`../../progressive-web-sdk/${folder}/**/*${ext}`, (err, files) => {
         if (err) {
             console.log(err)
         }
-
         files.forEach((file) => {
             fs.readFile(file, (err, data) => {
                 if (err) {
                     console.log(err)
                 }
                 if (!isCopyrightHeader(data)) {
-                    if (process.argv.indexOf('--verify') >= 0) {
-                        /* eslint-disable */
-                        console.log('Missing copyright headers in '.yellow + file.green)
-                        /* eslint-enable */
-                        arr.push(file);
-                    } else {
-                        const newData = getCopyrightHeader() + data
-                        debugger
-                        writeCopyrightHeader(file, newData)
-                    }
+                    debugger;
+                    const newData = getCopyrightHeader() + data
+                    writeCopyrightHeader(file, newData)
                 }
             })
         })
@@ -70,6 +60,41 @@ const checkHeaders = (dir) => {
 
 }
 
-process.argv.forEach((dir) => {
-    checkHeaders(dir)
-})
+const checkHeaders = (folder, ext) => {
+    glob(`../../progressive-web-sdk/${folder}/**/*${ext}`, (err, files) => {
+        let failed = false
+
+        if (err) {
+            console.log(err)
+        }
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i]
+            if (!isCopyrightHeader(fs.readFileSync(file))) {
+                console.log('Missing copyright headers in '.yellow + file.green)
+                failed = true
+            }
+        }
+
+        if (failed === true) {
+            console.log('ERR - please run the copyright header tool in this project'.red)
+            process.exit(1)
+        }
+    })
+}
+
+if (process.argv.indexOf('--verify') >= 0) {
+    process.argv.splice(process.argv.indexOf('--verify'), 1)
+
+    for (let i = 2; i < process.argv.length; i++) {
+        let dir = process.argv[i]
+        let lang = process.argv[process.argv.length - 1]
+        checkHeaders(dir, lang)
+    }
+
+    console.log('Copyright headers successful'.green);
+} else {
+    process.argv.forEach((dir) => {
+        updateHeaders(dir, process.argv[process.argv.length - 1]) // TODO make .ext variable from cli
+    })
+}
