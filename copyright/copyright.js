833b4a2c33a2d1bb1bbcdfef99b7987b7f07e130
Cdok <cdokurno@mobify.com>
2017-05-02 15:59:22 -0700

    copyright.run() function addition
    This commit moves the CL argument processing into a function "run",
    inside of the copyright object

--- copyright/copyright.js
+++ copyright/copyright.js
@@ -2,9 +2,13 @@ const glob = require('glob')
 const fs = require('fs')
 const colors = require('colors')
 
+// dev:
 // node --inspect --debug-brk copyright.js ../../progressive-web-sdk/bin/**/*.js
+
+// test:
 // node copyright.js ../../progressive-web-sdk/bin/**/*.js
 // node copyright.js ../../progressive-web-sdk/bin/**/*.js --lint
+
 const copyright = {
     items: [],
     ext: '',
@@ -14,7 +18,7 @@ const copyright = {
             fs.readFile(item, (err, data) => {
                 if (!this.hasCopyrightHeader(data)) {
                     if (this.lintMode === true) {
-                        console.log('ERROR - Please run the copyright header tool in this project'.red)
+                        console.log('ERROR: Missing Copyright Headers - Please run the copyright header tool in this project'.red)
                         process.exit(1)
                     } else {
                         this.writeHeader(item, data)
@@ -42,33 +46,39 @@ const copyright = {
                 console.log(err)
             }
         })
-    }
-}
+    },
+    run: function() {
+        // Sets lint flag if the user provides --lint command line arg
+        if (process.argv.indexOf('--lint') >= 0) {
+            process.argv.splice(process.argv.indexOf('--lint'), 1)
+            this.lintMode = true
+        }
 
-if (process.argv.indexOf('--lint') >= 0) {
-    process.argv.splice(process.argv.indexOf('--lint'), 1)
-    copyright.lintMode = true
-}
+        // Need to add case for when the user does not provide any glob strings
+        // i.e. node copyright.js
+        if (process.argv.length <= 2) {
+            console.log('Please enter a list of globs to add copyrights to, followed by an optional --lint command'.cyan)
+            console.log('example - node copyright.js src/**/*.js --lint'.yellow)
+        }
 
-if (process.argv.length <= 2) {
-    console.log('Please enter a list of globs to add copyrights to, followed by an optional --lint command'.cyan)
-    console.log('example - node copyright.js src/**/*.js --lint'.yellow)
-}
+        let argumentsProcessed = 0
 
-let argumentsProcessed = 0
+        process.argv.forEach(function(dir, index, args) {
+            if (!/node/.test(dir) && !/copyright/.test(dir)) {
+                copyright.setExtension(dir)
+                glob(`${dir}`, (err, files) => {
+                    copyright.items.push(files[0])
+                    argumentsProcessed++
 
-process.argv.forEach(function(dir, index, args) {
-    if (!/node/.test(dir) && !/copyright/.test(dir)) {
-        copyright.setExtension(dir)
-        glob(`${dir}`, (err, files) => {
-            copyright.items.push(files[0])
-            argumentsProcessed++
-
-            if (argumentsProcessed === args.length) {
-                copyright.updateHeaders()
+                    if (argumentsProcessed === args.length) {
+                        copyright.updateHeaders()
+                    }
+                })
+            } else {
+                argumentsProcessed++
             }
         })
-    } else {
-        argumentsProcessed++
     }
-})
+}
+
+copyright.run()
