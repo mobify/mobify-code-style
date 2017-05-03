771a71cf9c35f54a295eb8800a7687d1aa629b60
Cdok <cdokurno@mobify.com>
2017-05-03 10:39:04 -0700

    fix linting errors, better error logging
    this commit changes how command line arguments are handled,
    as well as fixing the ES6 linting errors. Additionally, this commit
    adds more detailed errors / messages when success or error states
    occur

--- copyright/copyright.js
+++ copyright/copyright.js
@@ -11,7 +11,7 @@ const copyright = {
     ext: '',
     lintMode: false,
     updateHeaders() {
-        const filesRead = this.items.map((item, index, args) => {
+        const filesRead = this.items.map((item) => {
             return new Promise((resolve) => {
                 fs.readFile(item, (err, data) => {
                     if (!this.hasCopyrightHeader(data)) {
@@ -19,26 +19,25 @@ const copyright = {
                             console.log(`\x1b[33m Missing copyright headers in \x1b[36m ${item}`)
                             this.missingHeaders.push(item)
                         } else {
+                            console.log(`\x1b[32mCopyright header succesfully written into \x1b[35m${item}`)
                             this.writeHeader(item, data)
                         }
                     }
-                    resolve(item)
+                    resolve()
                 })
             })
         })
 
-        Promise.all(filesRead).then((item) => {
+        Promise.all(filesRead).then(() => {
             if (this.lintMode === true) {
                 if (this.missingHeaders.length > 0) {
                     console.log('\x1b[31m \x1b[40mERROR\x1b[49m - Please run the copyright headers tool in this project')
                     process.exit(1)
                 } else {
-                    console.log('\x1b[36m Copyright Headers Present')
+                    console.log('\x1b[32m\x1b[40mSUCCESS\x1b[49m - \x1b[36m All copyright headers present')
                 }
             } else {
-                item.forEach((item) => {
-                    console.log(`\x1b[32mCopyright header succesfully written into \x1b[35m${item}`);
-                })
+                console.log('\x1b[32m\x1b[40mSUCCESS\x1b[49m - Copyright headers finished')
             }
         })
 
@@ -56,8 +55,9 @@ const copyright = {
     writeHeader(file, data) {
         const path = `headers/copyright${this.ext}.txt`
 
-        if (fs.existsSync(path)) {
+        if (!fs.existsSync(path)) {
             console.log(`\x1b[31m \x1b[40mERROR\x1b[49m - ${path} does not exist`)
+            process.exit(1)
         }
 
         const copyrightText = fs.readFileSync(path)
@@ -70,30 +70,31 @@ const copyright = {
         })
     },
     run() {
+        // removes first 2 args (node copyright.js)
+        const args = process.argv.slice(2)
+
         // Sets lint flag if the user provides --lint command line arg
-        if (process.argv.indexOf('--lint') >= 0) {
-            process.argv.splice(process.argv.indexOf('--lint'), 1)
+        if (args.indexOf('--lint') >= 0) {
+            args.splice(args.indexOf('--lint'), 1)
             this.lintMode = true
         }
 
         // Case for when the user does not provide any glob strings
         // i.e. node copyright.js
-        if (process.argv.length <= 2) {
+        if (args.length <= 2) {
             console.log('\x1b[36m Please enter a list of globs to add copyrights to, followed by an optional --lint command')
             console.log('\x1b[33m example - "node copyright.js src/**/*.js --lint"')
+            process.exit(0)
         }
 
-
-        const processedGlobs = process.argv.map((dir, index, args) => {
-            if (!/node/.test(dir) && !/copyright/.test(dir)) {
-                return new Promise((resolve) => {
-                    this.setExtension(dir)
-                    glob(`${dir}`, (err, files) => {
-                        this.items.push(files[0])
-                        resolve()
-                    })
+        const processedGlobs = args.map((dir) => {
+            return new Promise((resolve) => {
+                this.setExtension(dir)
+                glob(`${dir}`, (err, files) => {
+                    this.items.push(files[0])
+                    resolve()
                 })
-            }
+            })
         })
 
         Promise.all(processedGlobs).then(() => copyright.updateHeaders())
