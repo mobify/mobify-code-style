88a4a691c34927b206971b2c84a481e696bbc4c8
Cdok <cdokurno@mobify.com>
2017-05-04 14:34:28 -0700

    small refactor to remove copyright object wrapper
    This commit removes the need for the copyright object, and instead
    keeps all of the functions at the module-level, instead of bound to
    the copyright object

--- copyright/copyright.js
+++ copyright/copyright.js
@@ -17,62 +17,33 @@ const blackBG = '\x1b[40m'
 const defaultBG = '\x1b[49m'
 const defaultFG = '\x1b[39m'
 
+let langs = {}
+let lintMode = true
+let error = false
+
 const args = process.argv.filter((arg) => {
     return !/node|copyright/.test(arg)
 })
 
-const copyright = {
-    lintMode: true,
-    langs: {},
-    run() {
-        this.buildSupportedExtensions()
-        let error = false
-        args
-            .map((folder) => glob.sync(folder))
-            .reduce((a, b) => a.concat(b))
-            .forEach((file) => {
-                const content = fs.readFileSync(file)
-                const hasCopyrightHeader = content.includes('Copyright (c)')
-                const ext = file.match(/\.[0-9a-z]+$/i)[0]
-
-                if (!hasCopyrightHeader) {
-                    if (this.lintMode) {
-                        console.log(`${yellow}${file} ${red}missing copyright header`)
-                        error = true
-                    } else {
-                        const newData = this.getHeaderText(ext) + content
-                        fs.writeFileSync(file, newData)
-                        console.log(`${green}Copyright header succesfully written into ${magenta}${file}`)
-                    }
-                }
-            })
-
-        if (error) {
-            console.log(`${red}${blackBG}ERROR${defaultBG} - Please run the copyright headers tool in this project`)
-            process.exit(1)
-        } else {
-            console.log(`${cyan}Copyright headers are present in target files`)
-        }
-    },
-    getHeaderText(ext) {
-        if (!this.langs[ext]) {
-            console.log(`${red}${blackBG}ERROR${defaultBG} - ${ext} is not supported (yet)`)
-            process.exit(1)
-        } else {
-            return this.langs[ext]
-        }
-    },
-    buildSupportedExtensions() {
-        const headerDir = path.join(__dirname, './headers')
-        fs
-            .readdirSync(headerDir)
-            .forEach((file) => {
-                const extension = file.match(/\.[0-9a-z]+$/i)[0]
-                const textPath = path.join(headerDir, file)
-                const content = fs.readFileSync(textPath).toString().replace('year', new Date().getFullYear())
-                this.langs[extension] = content
-            })
+const getHeaderText = (ext) => {
+    if (!langs[ext]) {
+        console.log(`${red}${blackBG}ERROR${defaultBG} - ${ext} is not supported (yet)`)
+        process.exit(1)
+    } else {
+        return langs[ext]
     }
+}
+
+const buildSupportedExtensions = () => {
+    const headerDir = path.join(__dirname, './headers')
+    fs
+        .readdirSync(headerDir)
+        .forEach((file) => {
+            const extension = file.match(/\.[0-9a-z]+$/i)[0]
+            const textPath = path.join(headerDir, file)
+            const content = fs.readFileSync(textPath).toString().replace('year', new Date().getFullYear())
+            langs[extension] = content
+        })
 }
 
 if (args.length === 0 || args.indexOf('--help') >= 0) {
@@ -98,7 +69,34 @@ if (args.length === 0 || args.indexOf('--help') >= 0) {
 // Sets fix flag if the user provides --fix command line arg
 if (args.indexOf('--fix') >= 0) {
     args.splice(args.indexOf('--fix'), 1)
-    copyright.lintMode = false
+    lintMode = false
 }
 
-copyright.run()
+buildSupportedExtensions()
+
+args
+    .map((folder) => glob.sync(folder))
+    .reduce((a, b) => a.concat(b))
+    .forEach((file) => {
+        const content = fs.readFileSync(file)
+        const hasCopyrightHeader = content.includes('Copyright (c)')
+        const ext = file.match(/\.[0-9a-z]+$/i)[0]
+
+        if (!hasCopyrightHeader) {
+            if (lintMode) {
+                console.log(`${yellow}${file} ${red}missing copyright header`)
+                error = true
+            } else {
+                const newData = getHeaderText(ext) + content
+                fs.writeFileSync(file, newData)
+                console.log(`${green}Copyright header succesfully written into ${magenta}${file}`)
+            }
+        }
+    })
+
+if (error) {
+    console.log(`${red}${blackBG}ERROR${defaultBG} - Please run the copyright headers tool in this project`)
+    process.exit(1)
+} else {
+    console.log(`${cyan}Copyright headers are present in target files`)
+}
